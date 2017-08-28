e82e9b9c5f0cb95a76d3ae669e270c36b7c3d40c
Upshay <upsdmr@gmail.com>
2017-08-29 03:17:37 +1000

    Added ANT Design UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { DatePicker } from 'antd';
 import logo from './logo.svg';
 import './App.css';
+import { Tabs } from 'antd';
+const TabPane = Tabs.TabPane;
 
 class App extends Component {
   render() {
@@ -10,7 +13,17 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Wellness MasterKeys React JS App</h2>
         </div>
-        
+        <DatePicker/>
+          <Tabs defaultActiveKey="1" >
+    <TabPane tab="Tab 1" key="1">
+    Questionnaire 1 
+    </TabPane>
+    <TabPane tab="Tab 2" key="2">
+      Questionnaire 2
+    </TabPane>
+    <TabPane tab="Tab 3" key="3">
+      Questionnaire 3</TabPane>
+  </Tabs>
       </div>
     );
   }
